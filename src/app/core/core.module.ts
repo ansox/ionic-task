3760a75956b6382b50d92d58b59c6cd559461025
Anso <anderson.floriano@gmail.com>
2021-10-21 18:43:59 -0300

    Adding sign in and sign out methods

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
-  imports: [IonicModule.forRoot()],
+  imports: [
+    IonicModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   exports: [BrowserModule, IonicModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBu4WYHfbXQAN9QlChODpsCxwVdETeHWI4',
+    authDomain: 'ionic-firestore-tasks-c7129.firebaseapp.com',
+    projectId: 'ionic-firestore-tasks-c7129',
+    storageBucket: 'ionic-firestore-tasks-c7129.appspot.com',
+    messagingSenderId: '623558276190',
+    appId: '1:623558276190:web:e3f93f39183a58a24230cf',
+    measurementId: 'G-4CGTVQRMWD',
+  },
 };
 
 /*
